0a6776438d8bf4735feddd7c63eac6e6ee142ef4
Varun Kedia <purplevarun@gmail.com>
2022-07-28 19:18:10 +0530

    Initiated App

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+	return (
+		<View
+			style={{ height: "100%", alignItems: "center", justifyContent: "center" }}
+		>
+			<Text>Hello World</Text>
+		</View>
+	);
+};
+export default App;
